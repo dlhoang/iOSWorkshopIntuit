3f136a4b7a0b664d50bbe19525e923352199a2fd
Di Hoang <dlhoang@calpoly.edu>
2017-05-04 21:25:41 -0700

    added some error checks

--- TipCalculator/TipCalculator/SettingsViewController.swift
+++ TipCalculator/TipCalculator/SettingsViewController.swift
@@ -42,40 +42,51 @@ class SettingsViewController: UIViewController {
         /* if they entered a number, good to go */
         if Double(self.defaultTipField.text!) != nil {
             
-            invalidTipLabel.text = ""
-            
-            /* make sure user wants to set tip */
-            let alertController = UIAlertController(title: "Set Default Tip?", message:
-                "Default tip will be set to \(defaultTipField.text!)%.", preferredStyle: UIAlertControllerStyle.alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: {(action: UIAlertAction!) in
+            if (Double(self.defaultTipField.text!)! >= 0.0) {
+                invalidTipLabel.text = ""
                 
-                let defaultTip = Double(self.defaultTipField.text!)
-            
-                // Set a Double value for some key.
-                self.defaults.set(defaultTip, forKey: "defaultTip")
-            
-                // Force UserDefaults to save.
-                self.defaults.synchronize()
-            }))
-            
-            alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler:nil))
-            
-            self.present(alertController, animated: true, completion: nil)
+                /* make sure user wants to set tip */
+                let alertController = UIAlertController(title: "Set Default Tip?", message:
+                    "Default tip will be set to \(defaultTipField.text!)%.", preferredStyle: UIAlertControllerStyle.alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: {(action: UIAlertAction!) in
+                    
+                    let defaultTip = Double(self.defaultTipField.text!)
+                
+                    // Set a Double value for some key.
+                    self.defaults.set(defaultTip, forKey: "defaultTip")
+                
+                    // Force UserDefaults to save.
+                    self.defaults.synchronize()
+                }))
+                
+                alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler:nil))
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+            else {
+                invalidAmountAnimation(textfield: defaultTipField, message: "Amount must be positive")
+            }
         }
         
         /* if they didn't enter number, don't let them save it */
         else {
-            let animation = CABasicAnimation(keyPath: "position")
-            animation.duration = 0.08
-            animation.repeatCount = 1
-            animation.autoreverses = true
-            animation.fromValue = NSValue(cgPoint: CGPoint(x: defaultTipField.center.x - 10, y: defaultTipField.center.y))
-            animation.toValue = NSValue(cgPoint: CGPoint(x: defaultTipField.center.x + 10, y: defaultTipField.center.y))
-            defaultTipField.layer.add(animation, forKey: "position")
-            
-            invalidTipLabel.text = "Tip amount must be a number"
-            
+            invalidAmountAnimation(textfield: defaultTipField, message: "Tip amount must be a number")
         }
+    }
+    
+    /* springing animation for invalid textfield */
+    func invalidAmountAnimation(textfield: UITextField, message: String) {
+        
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.08
+        animation.repeatCount = 1
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: textfield.center.x - 10, y: textfield.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: textfield.center.x + 10, y: textfield.center.y))
+        textfield.layer.add(animation, forKey: "position")
+        
+        invalidTipLabel.text = message
+        
     }
 }
